1a3e7ef8bb3ffaed9c2715732bed8b966f055bcc
Faruk İpek <farukipekweb@gmail.com>
2022-11-05 17:48:02 -0400

    upload dummy data to blog page

--- components/post/post.js
+++ components/post/post.js
@@ -1,15 +1,13 @@
-import styles from './post.module.scss'
-import ArrowRight from '../icons/arrow-right';
-const Post = ({title,date}) => {
-    return (
-       <a href="post" class={styles.post}>
-            <h3 class={styles.title}>
-              {title}
-            </h3>
-            <ArrowRight size={15}/>
-            <span class={styles.date}>{date}</span>
-          </a>
-    );
-}
+import styles from "./post.module.scss";
+import ArrowRight from "../icons/arrow-right";
+const Post = ({ title, date, url }) => {
+  return (
+    <a href={url} class={styles.post}>
+      <h3 class={styles.title}>{title}</h3>
+      <ArrowRight size={15} />
+      <span class={styles.date}>{date}</span>
+    </a>
+  );
+};
 
 export default Post;
--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,34 @@
 /* eslint-disable react/no-unescaped-entities */
 import Title from "../components/title/title";
-import Post from '../components/post/post';
-import Search from '../components/search/search'
+import Post from "../components/post/post";
+import Search from "../components/search/search";
 const Blog = () => {
+  const blog = [
+    {
+      id: 1,
+      title: "Quisque congue felis id dictum posuere.",
+      date: "September 2, 2022",
+      url: "post",
+    },
+    {
+      id: 2,
+      title: "Integer feugiat metus nec nisl pharetra ullamcorper.",
+      date: "June 24, 2022",
+      url: "post",
+    },
+    {
+      id: 3,
+      title: "Sed eget libero eu tellus consectetur cursus a eu ante.",
+      date: "May 11, 2022",
+      url: "post",
+    },
+    {
+      id: 4,
+      title: "Curabitur blandit tortor vel urna vehicula iaculis.",
+      date: "April 3, 2022",
+      url: "post",
+    },
+  ];
   return (
     <div class="blog">
       <div class="container">
@@ -12,10 +38,13 @@ const Blog = () => {
         />
         <Search />
         <div class="blog-list">
-         <Post title={"Quisque congue felis id dictum posuere."} date={"September 2, 2022"} />
-         <Post title={"Integer feugiat metus nec nisl pharetra ullamcorper."} date={"June 24, 2022"} />
-         <Post title={"Sed eget libero eu tellus consectetur cursus a eu ante."} date={"May 11, 2022"} />
-         <Post title={"Curabitur blandit tortor vel urna vehicula iaculis."} date={"April 3, 2022"} />
+          {blog.map((item) => {
+            return (
+              <>
+                <Post title={item.title} date={item.date} url={item.url} />
+              </>
+            );
+          })}
         </div>
       </div>
     </div>

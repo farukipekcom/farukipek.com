d951567cf7f615aa1c559fb18eb08a5690bb496a
Faruk İpek <farukipekweb@gmail.com>
2022-11-05 16:48:58 -0400

    add icons

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@
 import Title from "../components/title/title";
 import Post from '../components/post/post';
 import ProjectCard from "../components/project-card/project-card";
+import ArrowRight from "../components/icons/arrow-right";
 export default function Home() {
   return (
     <>
@@ -21,14 +22,7 @@ export default function Home() {
             <p className="homepage-heading">Check out my latest articles</p>
             <a href="blog" className="homepage-link">
               Browse all blog posts
-              <svg
-                width="15"
-                height="11"
-                viewBox="0 0 15 11"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M14.1759 4.89909L9.85794 0.864664C9.46002 0.492866 8.79688 0.492866 8.39896 0.864664C8.00104 1.23646 8.00104 1.85606 8.39896 2.22786L10.9485 4.62368L1.51691 4.62368C0.942169 4.62368 0.485352 5.0505 0.485352 5.58751C0.485352 6.12451 0.942169 6.55134 1.51691 6.55134L10.9485 6.55134L8.39896 8.93342C8.00104 9.30521 8.00104 9.92481 8.39896 10.2966C8.60527 10.4894 8.87059 10.5858 9.13581 10.5858C9.40102 10.5858 9.66633 10.4894 9.87265 10.2966L14.1758 6.27595C14.3674 6.09692 14.4854 5.84913 14.4854 5.58748C14.4854 5.32584 14.3821 5.07804 14.1758 4.89902L14.1759 4.89909Z" />
-              </svg>
+             <ArrowRight size={15} />
             </a>
             <div className="homepage-blog-list">
             <Post title={"Quisque congue felis id dictum posuere."} date={"September 2, 2022"} />
@@ -44,14 +38,7 @@ export default function Home() {
             </p>
             <a href="projects" className="homepage-link">
               Browse all projects
-              <svg
-                width="15"
-                height="11"
-                viewBox="0 0 15 11"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M14.1759 4.89909L9.85794 0.864664C9.46002 0.492866 8.79688 0.492866 8.39896 0.864664C8.00104 1.23646 8.00104 1.85606 8.39896 2.22786L10.9485 4.62368L1.51691 4.62368C0.942169 4.62368 0.485352 5.0505 0.485352 5.58751C0.485352 6.12451 0.942169 6.55134 1.51691 6.55134L10.9485 6.55134L8.39896 8.93342C8.00104 9.30521 8.00104 9.92481 8.39896 10.2966C8.60527 10.4894 8.87059 10.5858 9.13581 10.5858C9.40102 10.5858 9.66633 10.4894 9.87265 10.2966L14.1758 6.27595C14.3674 6.09692 14.4854 5.84913 14.4854 5.58748C14.4854 5.32584 14.3821 5.07804 14.1758 4.89902L14.1759 4.89909Z" />
-              </svg>
+             <ArrowRight size={15} />
             </a>
             <div className="homepage-projects-list">
               <ProjectCard title={"Quisque congue"} category={"WEB DESIGN + UI DESIGN + SEO"}/>

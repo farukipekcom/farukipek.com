83cb972281f0fe6e901f69ad351b9fbffefcdaed
Faruk İpek <farukipekweb@gmail.com>
2022-11-05 14:46:49 -0400

    fix title

--- components/title/title.js
+++ components/title/title.js
@@ -1,10 +1,12 @@
 import styles from "./title.module.scss";
-const Title = ({ pagetitle, title,details }) => {
+import Button from '../button/button'
+const Title = ({ pagetitle, title, details, hireme }) => {
   return (
     <>
       <div className={styles.pageTitle}>{pagetitle}</div>
       <h1 className={styles.title}>{title}</h1>
       <h1 className={styles.details}>{details}</h1>
+      {hireme && <Button text={"Hire Me"} />}
     </>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -12,20 +12,8 @@ export default function Home() {
             details={
               "I've been building stuff on the web for the last 6 years. I've worked with clients in various countries and industries."
             }
+            hireme={1}
           />
-          <div className="hero">
-            <a href="contact" className="homepage-hireme">
-              <span>Hire Me</span>
-              <svg
-                width="15"
-                height="11"
-                viewBox="0 0 15 11"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M14.1759 4.89909L9.85794 0.864664C9.46002 0.492866 8.79688 0.492866 8.39896 0.864664C8.00104 1.23646 8.00104 1.85606 8.39896 2.22786L10.9485 4.62368L1.51691 4.62368C0.942169 4.62368 0.485352 5.0505 0.485352 5.58751C0.485352 6.12451 0.942169 6.55134 1.51691 6.55134L10.9485 6.55134L8.39896 8.93342C8.00104 9.30521 8.00104 9.92481 8.39896 10.2966C8.60527 10.4894 8.87059 10.5858 9.13581 10.5858C9.40102 10.5858 9.66633 10.4894 9.87265 10.2966L14.1758 6.27595C14.3674 6.09692 14.4854 5.84913 14.4854 5.58748C14.4854 5.32584 14.3821 5.07804 14.1758 4.89902L14.1759 4.89909Z" />
-              </svg>
-            </a>
-          </div>
           <div className="homepage-blog">
             <h2 className="shadow">BLOG</h2>
             <p className="homepage-heading">Check out my latest articles</p>

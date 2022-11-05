6cb429ae9134d8c5da158a2880394e5168fd722c
Faruk İpek <farukipekweb@gmail.com>
2022-11-04 21:15:37 -0400

    add header component

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,5 +1,11 @@
+import Header from "../header/header";
 const Layout = ({ children }) => {
-  return <>{children}</>;
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
 };
 
 export default Layout;

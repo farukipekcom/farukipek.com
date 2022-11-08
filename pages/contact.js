e9138332fd25d42d06f02ad01919c338d1fb25ac
Faruk İpek <farukipekweb@gmail.com>
2022-11-08 12:38:34 -0500

    fix contact form

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,21 @@
 import styles from "./contact.module.scss";
 import Title from "../components/title/title";
 const Contact = () => {
+  async function handleOnSubmit(e) {
+    e.preventDefault();
+
+    const formData = {};
+
+    Array.from(e.currentTarget.elements).forEach((field) => {
+      if (!field.name) return;
+      formData[field.name] = field.value;
+    });
+
+    await fetch("/api/mail", {
+      method: "POST",
+      body: JSON.stringify(formData),
+    });
+  }
   return (
     <div className={styles.contact}>
       <div className={styles.container}>
@@ -11,13 +26,14 @@ const Contact = () => {
             "Reach out filling the form below or send me an email at faruk@farukipek.com"
           }
         />
-        <form className={styles.form}>
+        <form className={styles.form} method="post" onSubmit={handleOnSubmit}>
           <div className={styles.item}>
             <span className={styles.label}>Name</span>
             <input
               type="text"
               placeholder="Your name"
               className={styles.input}
+              name="name"
             />
           </div>
           <div className={styles.item}>
@@ -26,12 +42,13 @@ const Contact = () => {
               type="text"
               placeholder="you@company.com"
               className={styles.input}
+              name="email"
             />
           </div>
           <div className={styles.item}>
             <span className={styles.label}>Message</span>
             <textarea
-              name=""
+              name="message"
               id=""
               placeholder="Tell me about your site project requirements and when you'd like to start."
               className={`${styles.input} ${styles.textarea}`}

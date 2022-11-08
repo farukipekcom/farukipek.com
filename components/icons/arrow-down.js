fd919c9297f7676237516d7410ed5d3aa3c82cc5
Faruk İpek <farukipekweb@gmail.com>
2022-11-08 10:02:58 -0500

    fix icons

--- components/icons/arrow-down.js
+++ components/icons/arrow-down.js
@@ -5,7 +5,6 @@ const ArrowDown = ({ size = 24, gonder }) => {
       height={size}
       viewBox="0 0 16 10"
       xmlns="http://www.w3.org/2000/svg"
-      className={gonder}
     >
       <path d="M7.99996 9.75999L0 1.76003L1.76003 0L8 6.23997L14.24 0L16 1.76003L7.99996 9.75999Z" />
     </svg>

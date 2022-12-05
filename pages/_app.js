1d7e222aac30a1193a5b317e27a405101de7557a
Faruk İpek <farukipekweb@gmail.com>
2022-12-05 18:56:27 -0500

    feat: add google analytic code

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import "../styles/main.scss";
+import Script from "next/script";
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        async
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-15CY4D1X90"
+      />
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-15CY4D1X90');`}
+      </Script>
       <Component {...pageProps} />
     </>
   );

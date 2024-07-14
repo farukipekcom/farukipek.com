2ef61a927e6d52ce52ca7977a865c4423fe765db
farukipekcom <farukipekweb@gmail.com>
2024-07-14 14:59:49 -0500

    updated footer menu and links

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,30 +1,43 @@
 import React from "react";
-
 import Twitter from "./icons/Twitter";
 import Instagram from "./icons/Instagram";
 import Github from "./icons/Github";
 import Linkedin from "./icons/Linkedin";
-
+import Link from "next/link";
 export default function Footer() {
   return (
     <footer>
       <div className="footer-top">
         <div className="footer-heading">
           <div className="footer-heading-title">Have a project in mind?</div>
-          <div className="footer-heading-link">Let's talk about it!</div>
+          <a href="mailto:faruk@farukipek.com" className="footer-heading-link">
+            Let's talk about it!
+          </a>
         </div>
         <div className="footer-menu">
           <div className="footer-title">Explore</div>
           <div className="footer-menu-list">
-            <div className="footer-menu-list-item">Home</div>
-            <div className="footer-menu-list-item">About</div>
-            <div className="footer-menu-list-item">Blog</div>
-            <div className="footer-menu-list-item">Photos</div>
-            <div className="footer-menu-list-item">Projects</div>
+            <Link href="/" className="footer-menu-list-item">
+              Home
+            </Link>
+            <Link href="/about" className="footer-menu-list-item">
+              About
+            </Link>
+            <Link href="/blog" className="footer-menu-list-item">
+              Blog
+            </Link>
+            <Link href="/photos" className="footer-menu-list-item">
+              Photos
+            </Link>
+            <Link href="/projects" className="footer-menu-list-item">
+              Projects
+            </Link>
           </div>
           <div className="footer-contact">
             <div className="footer-title">Get in touch</div>
-            <div className="footer-email">faruk@farukipek.com</div>
+            <a href="mailto:faruk@farukipek.com" className="footer-email">
+              faruk@farukipek.com
+            </a>
           </div>
         </div>
       </div>

0d356ffe14c8c3f0e6d5088cfc3c30bfa49e0b71
farukipekcom <farukipekweb@gmail.com>
2024-07-17 12:22:10 -0500

    add new hover to post card

--- app/components/Blog/PostCard.jsx
+++ app/components/Blog/PostCard.jsx
@@ -9,9 +9,15 @@ export default function PostCard({ title, date, slug }) {
       className={`${styles.post} group`}
     >
       <div className={styles.container}>
-        <div className={`${styles.title} group-hover:text-white`}>{title}</div>
-        <div className={styles.line}></div>
-        <div className={`${styles.date} group-hover:text-white`}>
+        <div
+          className={`${styles.title} group-hover:mr-4 group-hover:translate-x-4 group-hover:text-white`}
+        >
+          {title}
+        </div>
+        <div className={`${styles.line} group w-[calc(100%)]`}></div>
+        <div
+          className={`${styles.date} group-hover:ml-4 group-hover:-translate-x-4 group-hover:text-white`}
+        >
           {format(date, "LLLL d, yyyy")}
         </div>
       </div>
